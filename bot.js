06b9e516648e34c79f77998c6bf7958e300998da
Daniel McNelis <dwm253@gmail.com>
2020-02-26 22:58:52 -0500

    f

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 
-const Discord = require('discord.js')
+//HEAD
+const Discord = require('./discord.js')
 const client = new Discord.Client()
 const fs = require('fs')
 const serverID = '682361248532398143'
