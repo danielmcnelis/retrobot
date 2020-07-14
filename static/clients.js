4a88a51ee3b9ce1f28f5dda8ef34b1c25a1fdc12
Daniel McNelis <dwm253@gmail.com>
2020-07-13 22:22:07 -0400

    removed secrets

--- static/clients.js
+++ static/clients.js
@@ -1,10 +1,10 @@
 
 const challonge = require('challonge')
 const Discord = require('discord.js')
+const { challongeAPIKey, discordBotToken} = require('../secrets.json')
 const client = new Discord.Client()
-const challongeClient = challonge.createClient({ apiKey: '4oKfYy4eD7Yw8RUDsRzN5rzLQ4fqPPjvZzgLn7YC' })
-client.login('NzMwOTIyMDAzMjk2NDE5ODUw.XweiXg.eXr2_PZNvN0B35lfbJNy5J-LUXE')
-
+const challongeClient = challonge.createClient({ apiKey: challongeAPIKey })
+client.login(discordBotToken)
 
 module.exports = {
     client,
